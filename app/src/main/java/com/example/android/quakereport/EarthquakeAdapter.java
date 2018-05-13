bced1de4cc7362021c4006809501d2c6816b0041
jlehocz <jozsef@lehocz.hu>
2018-05-13 22:00:14 +0200

    +Magnitude background circle

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -16,6 +16,7 @@
 package com.example.android.quakereport;
 
 import android.app.Activity;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +25,7 @@ import android.widget.TextView;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import android.graphics.drawable.GradientDrawable;
 
 /*
  * {@link AndroidFlavorAdapter} is an {@link ArrayAdapter} that can provide the layout for each list
@@ -54,6 +56,46 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         super(context, 0, Earthquakes);
     }
 
+    private int getMagnitudeColor(double magnitude) {
+        int magnitudeColorResourceId;
+        int magnitudeFloor = (int) Math.floor(magnitude);
+        switch (magnitudeFloor) {
+            case 0:
+            case 1:
+                magnitudeColorResourceId = R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColorResourceId = R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColorResourceId = R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColorResourceId = R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColorResourceId = R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColorResourceId = R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColorResourceId = R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColorResourceId = R.color.magnitude9;
+                break;
+            default:
+                magnitudeColorResourceId = R.color.magnitude10plus;
+                break;
+        }
+        return ContextCompat.getColor(getContext(), magnitudeColorResourceId);
+    }
+
+
     /**
      * Provides a view for an AdapterView (ListView, GridView, etc.)
      *
@@ -71,6 +113,7 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
             listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.earhtquake_list_item, parent, false);
         }
+
 
         // Get the {@link AndroidFlavor} object located at this position in the list
         Earthquake currentEarthQuake = getItem(position);
@@ -95,6 +138,18 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
             locationOffset=locationParts[0];
             primarylocation=locationParts[1];
         }
+
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable.
+        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeTextView.getBackground();
+
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor(currentEarthQuake.getMagnitude());
+
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
+
+
 
         // Find the TextView in the list_item.xml layout with the ID version_name
         TextView locationOffsetTextView = (TextView) listItemView.findViewById(R.id.location_offset);
